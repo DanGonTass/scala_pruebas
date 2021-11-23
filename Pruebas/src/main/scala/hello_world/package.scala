2a2abb88755d3a4e9550b946f1114213797656db
DanGonTass <dangontass@gmail.com>
2021-11-23 16:28:51 +0100

    Primer Commit

---
+++ Pruebas/src/main/scala/hello_world/package.scala
@@ -0,0 +1,12 @@
+package object hello_world {
+
+
+// esto es una prueba con github Autopilot
+
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+  }
+}
+
+}
